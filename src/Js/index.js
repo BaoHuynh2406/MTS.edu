40b09e599f1790cc2d47f7cf2087c5289f741da0
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-02-20 00:03:06 +0700

    update

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,28 +1,45 @@
-var m = angular.module('indexPage', []);
-m.controller('ctrlUser', function ($scope, $http) {
-    $scope.listStudents = []; //Mãng rỗng
-            // dùng giao thức http lấy mãng js về
-            $http.get('./src/db/Students.js').then(
-                function (d) {
-                    $scope.listStudents = d.data;
-                    $scope.name =  $scope.listStudents[0].fullname;
-                    $scope.marks =  $scope.listStudents[0].marks;
-                },
-                function (e) {
-                    alert('Lỗi: ' + e.statusText)
-                }
-            );
-});
+var app = angular.module('indexPage', ["ngRoute"]);
 
-m.controller('khoaHoc', ($scope, $http) =>{
-        $scope.Subjects = []; //Mãng rỗng
-         // dùng giao thức http lấy mãng js về
-         $http.get('./src/db/Subjects.js').then(
-            function (d) {
-                $scope.Subjects = d.data;
-            },
-            function (e) {
-                alert('Lỗi: ' + e.statusText)
-            }
-        );
-});
+    app.controller('ctrl', ($scope, $rootScope, $http, $location) =>{
+
+    });
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+          .when("/login", { templateUrl: "./login.html" })
+          .when("/lienhe", { templateUrl: "lienhe.html" })
+          .when("/tracnghiem/:idMH/:tenMH", { templateUrl: "tracnghiem.html", controller: "tnctrl" })
+          .otherwise({templateUrl: "./home.html"});
+      });
+
+
+
+
+    app.controller('ctrlUser', function ($scope, $http) {
+        $scope.listStudents = []; //Mãng rỗng
+                // dùng giao thức http lấy mãng js về
+                $http.get('./src/db/Students.js').then(
+                    function (d) {
+                        $scope.listStudents = d.data;
+                        $scope.name =  $scope.listStudents[0].fullname;
+                        $scope.marks =  $scope.listStudents[0].marks;
+                    },
+                    function (e) {
+                        alert('Lỗi: ' + e.statusText)
+                    }
+                );
+    });
+
+// m.controller('khoaHoc', ($scope, $http) =>{
+//         $scope.Subjects = []; //Mãng rỗng
+//          // dùng giao thức http lấy mãng js về
+//          $http.get('./src/db/Subjects.js').then(
+//             function (d) {
+//                 $scope.Subjects = d.data;
+//             },
+//             function (e) {
+//                 alert('Lỗi: ' + e.statusText)
+//             }
+//         );
+// });
+
